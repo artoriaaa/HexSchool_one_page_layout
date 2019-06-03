35f79c127a9082b8fdb60a0bee832a3edb6483ee
Milla <artoriaaa@gmail.com>
2019-06-03 21:07:00 +0800

    light box & top bottom

--- js/all.js
+++ js/all.js
@@ -5,5 +5,53 @@ $(document).ready(function() {
 		$(this).find('ul').stop().slideToggle(600);
 	});
 
-	
+	//top效果
+	$('.top a').click(function(event) {
+		event.preventDefault();
+		$('html, body').animate({
+			scrollTop: 0,
+		}, 800 );
+	});
+
+
+
+
+
+
+
+
+
+
+//lightbox
+ lightbox.option({
+  'resizeDuration': 200,
+  'wrapAround': true,
+  'positionFromTop': 200,
+  'showImageNumberLabel':false
+ })
+//swiper
+ //初始化
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    effect:'coverflow',
+    speed:1500,
+    autoplay:{
+    	delay:3000,
+    },
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      dynamicBullets: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+      dynamicBullets: true,
+    },
+  })
 });
